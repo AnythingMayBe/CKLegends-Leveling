71ccb54bf82e170b36a3acd10972b35d1f601f2c
AnythingMayBe <anythingmaybe@protonmail.com>
2022-01-03 18:19:15 +0100

    Added onrunmessages, shards in config and inited program

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ config = {
     # GLOBAL CONFIG
     "token": "DISCORD_BOT_TOKEN", # Replace that value with your Discord bot token, available on https://discord.com/developers/applications
     "prefix": "--", # Replace that value with the prefix of your bot, for example . or /
+    "onRunMessages": True, # Replace that with "False" if you don't want to get a message when it will run, with "True" if you want to.
+    "shards": 1, # Replace that with the value you want for shards (I recommend using one per 100 servers)
 
     # MESSAGES CONFIG
     "addMessageMin": 3, # Replace that value with the minimum amount of points a person will get per message
@@ -16,3 +18,9 @@ config = {
 }
 
 ### PROGRAM
+import discord
+from discord.ext import commands
+
+bot = commands.AutoShardedBot(command_prefix=config["prefix"], shard_count=config["shards"])
+
+bot.run(config["token"])
