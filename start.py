fdaa3a8bbdb723e668dbe25983dc4a60259edd74
AnythingMayBe <anythingmaybe@protonmail.com>
2022-01-05 18:34:10 +0100

    Added start.py for making the bot can load forever

---
+++ start.py
@@ -0,0 +1,6 @@
+# Don't forget to set your start command between system(" and ")
+# For example if you're on Windows, you may use "python main.py"
+
+from os import system
+while True:
+    system("python3 main.py")
